e1c0825fcf3edbd8ad8e0d377d3dce58ca08089c
Adam R <adam@adamrudd.net>
2021-09-07 13:47:40 +1000

    force selenium to point to binary path
    removed old chromium binary and replaced with one which can give us the location of the binary file

--- src/query/selenium_query.py
+++ src/query/selenium_query.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-import chromedriver_binary
+from chromedriver_py import binary_path
+import time
 
 def stock_check(url, text_string=None, div_id=None, div_class=None):
     """ use headless browser"""
@@ -8,12 +9,16 @@ def stock_check(url, text_string=None, div_id=None, div_class=None):
     #pip install chromedriver-binary==2.37.0
     #match_reponse = request_with_selenium(url="https://www.xxxxxxx.com.au/p/irobot-roomba-6-6-robot-vacuum-cleaner-r6-6/63387858", div_class="AddCart" ).text
     
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--window-size={}x{}'.format(1280, 1024))
-
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get(url)
+    # chrome_options = webdriver.ChromeOptions()
     
+    # chrome_options.add_argument('--window-size={}x{}'.format(1280, 1024))
+    # chrome_options.add_argument(f'--executable_path={binary_path}')
+
+    
+    driver = webdriver.Chrome(executable_path=binary_path)
+    # driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get(url)
+    match = None
     if div_id != None:
         try:
             match = driver.find_element_by_id(div_id).text
@@ -24,5 +29,10 @@ def stock_check(url, text_string=None, div_id=None, div_class=None):
             match = driver.find_element_by_class_name(div_class).text
         except:
             match = None
+    
+    time.sleep(5)
     driver.quit()
     return match
+
+
+stock_check('https://google.com')
