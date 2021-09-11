13fa63054b36445854f788a63e1ba1ae2d975956
Adam R <adam@adamrudd.net>
2021-09-11 18:01:39 +1000

    force ubuntu image in cloud create

--- bootscript.sh
+++ bootscript.sh
@@ -18,7 +18,18 @@ apt install -y python3-pip
 
 
 # Adding trusting keys to apt for repositories - needed for chrome install
-# wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
+
+#########
+
+# # install manually all the missing libraries
+# apt-get install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
+
+# # install chrome
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
+
+#########
 
 # Adding Google Chrome to the repositories
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
