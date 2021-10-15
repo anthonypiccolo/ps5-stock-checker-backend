b8f50b2da0dffb6b368814c467047b497d4effe0
Anthony Piccolo <anthonyjpiccolo@gmail.com>
2021-10-16 02:22:07 +1100

    Fix ze code

--- src/query/selenium_query.py
+++ src/query/selenium_query.py
@@ -1,5 +1,5 @@
 from os import curdir
-from seleniumwire import webdriver
+#from seleniumwire import webdriver
 #from selenium.webdriver.chrome.options import Options
 #import undetected_chromedriver.v2 as uc
 from seleniumwire.undetected_chromedriver.v2 import Chrome, ChromeOptions
@@ -32,16 +32,16 @@ import requests
 def set_chrome_settings():
     """ set up the chrome settings """
     #interesting thread here https://intoli.com/blog/making-chrome-headless-undetectable/
-    chrome_options = Options()
+    options = uc.ChromeOptions()
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument(f'user-agent={user_agent}')
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1920,1200")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    options.add_argument(f'user-agent={user_agent}')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1200")
     
-    return chrome_options
+    return options
 
 def notify(text_content):
     url = 'https://discord.com/api/webhooks/898508639319523328/Yrk-07ixQ09MW5gyAC0FFqxaXuG2l2lvn3wCMeMvCuYOP3GSNWht0MgAv401ggsmPoyx'
@@ -59,7 +59,7 @@ def stock_check(url, text_string=None, div_id=None, div_class=None, store=None):
     # chrome_options = webdriver.ChromeOptions()
     print(f"looking at website {url}...")
     #driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver",chrome_options=set_chrome_settings()) #seleniumwire_options=options)
-    driver = uc.Chrome()
+    driver = uc.Chrome(options=set_chrome_settings())
     driver.get(url)
     logging.info(f"store: {store}: {driver.page_source[0:75]}") #log out top 75 chars of page
     notify(f"store: {store}: {driver.page_source[0:75]}")
