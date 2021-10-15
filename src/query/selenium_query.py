a150496af7026eb2f511288ef94200a0d4a4383a
Anthony Piccolo <anthonyjpiccolo@gmail.com>
2021-10-16 01:29:18 +1100

    Reverting to headless mode

--- src/query/selenium_query.py
+++ src/query/selenium_query.py
@@ -34,7 +34,7 @@ def set_chrome_settings():
     #interesting thread here https://intoli.com/blog/making-chrome-headless-undetectable/
     chrome_options = Options()
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument(f'user-agent={user_agent}')
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
