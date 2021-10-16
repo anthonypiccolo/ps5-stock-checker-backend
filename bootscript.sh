643be597e339c636ed2ff609b8859682ef468d71
Adam R <adam@adamrudd.net>
2021-10-16 23:48:24 +1100

    pull in the metadata and assign to env var as part of boot script

--- bootscript.sh
+++ bootscript.sh
@@ -32,6 +32,8 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 
 #########
 
+
+
 # Adding Google Chrome to the repositories
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
@@ -71,6 +73,12 @@ apt install -y python3-pip
 
 pip3 install --upgrade pip
 pip3 install --ignore-installed -r /app/requirements.txt
+
+##########################
+## Assign env variables ##
+##########################
+
+export PROXY_CREDS=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/PROXY_CREDS" -H "Metadata-Flavor: Google")
 
 
 
